2c4d8fcbd95b25569983b6f95b3e957ee641207b
steveg <codechange@garran.com>
2017-03-09 13:39:55 -0700

    New Program - Create Assembly File

---
+++ CreateAssemblyFile/CreateAssemblyFile/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreateAssemblyFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Get Filename
+            // Create outputfile.hack
+            // create Assembler instancej
+             
+            // do assembler pass1
+            // Do assembler pass2
+            // close the files
+        }
+    }
+}
